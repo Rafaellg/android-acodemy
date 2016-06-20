85114b163ec430ec0b90fae2e780c669f33527f4
Rafael Guimaraes <rafaellguimas@gmail.com>
2016-06-20 00:38:09 -0300

    Correções de layout.

--- app/src/main/java/com/kreativ/rafael/androidschool/activity/AboutActivity.java
+++ app/src/main/java/com/kreativ/rafael/androidschool/activity/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.kreativ.rafael.androidschool.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.kreativ.rafael.androidschool.R;
 
@@ -11,5 +15,43 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        FloatingActionButton btnGooglePlus = (FloatingActionButton) findViewById(R.id.btnGooglePlus);
+        FloatingActionButton btnLinkedin = (FloatingActionButton) findViewById(R.id.btnLinkedin);
+        FloatingActionButton btnFacebook = (FloatingActionButton) findViewById(R.id.btnFacebook);
+
+        btnGooglePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to github project
+                String url = "https://plus.google.com/u/0/+RafaelGuimaraes";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        btnLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to github project
+                String url = "https://www.linkedin.com/in/rafaellguimaraes";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to github project
+                String url = "https://www.facebook.com/rafaellguimas";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
     }
 }
